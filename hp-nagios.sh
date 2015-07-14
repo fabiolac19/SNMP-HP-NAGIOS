60ce6ce890f0c155cfe02b99cdbbf3528bb0cb25
Fabiola Campos <fabiolac19@gmail.com>
2015-07-14 15:30:40 -0300

    Script para monitoreo de cluster storage HP StoreVirtual con SNMP

--- hp-nagios.sh
+++ hp-nagios.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-# Modo de uso: hp-nagios.sh <ip addr or hostname> <private or public>
+# Script para monitoreo de cluster storage HP StoreVirtual con SNMP.
+# Monitorea capacidad ocupada/disponible/total - Muestra en BG y MG
+# El usuario puede ingresar valores de umbral critico y de alerta para la evaluacion del estado del cluster
 
-#----------
+# Modo de uso: hp-nagios.sh <ip/hostname> <priv/publ> <umbral de alerta> <umbral critico> <instancia o cluster a consultar>
 
+# Define variables de retorno para el estado del cluster
 STATE_OK=$(expr 0)
 STATE_WARNING=$(expr 1)
 STATE_CRITICAL=$(expr 2)
 STATE_UNKNOWN=$(expr 3)
 CLUSTERINSTANCE=$5
-
-
+# LIBEXEC= ruta de acceso a pluggins o addons de NAGIOS donde se encuentra check_snmp
 LIBEXEC="/usr/local/icinga/libexec"
 
 RET=$?
@@ -20,54 +22,52 @@ echo "query problem - No data received from host"
 exit $STATE_UNKNOWN
 fi
 
-# figure out the name of the cluster for instance supplied on command line
-clustername=$($LIBEXEC/check_snmp -P 2c -H $1 -C $2 -o LEFTHAND-NETWORKS-NSM-CLUSTERING-MIB::clusClusterName.$CLUSTERINSTANCE|cut -d" " -f4)
-#echo "Clustername=$clustername"
+# Usage:check_snmp [-P snmp version] -H <ip_address> [-C community] -o <OID>
+# -P, --protocol=[1|2c|3], SNMP protocol version
+# -H, --hostname=ADDRESS, Host name, IP Address, or unix socket (must be an absolute path)
+# -C, --community=STRING, Optional community string for SNMP communication (default is "public")
+# -o, --oid=OID(s), Object identifier(s) or SNMP variables whose value you wish to query
 
-# total number of modules in management group
+# the name of the cluster for instance supplied on command line CLUSTERINSTANCE=$5
+clustername=$($LIBEXEC/check_snmp -P 2c -H $1 -C $2 -o LEFTHAND-NETWORKS-NSM-CLUSTERING-MIB::clusClusterName.$CLUSTERINSTANCE|cut -d" " -f4)
+echo "Clustername=$clustername"
+
+# total number of modules in management group. network storage modules in this system
 totalmodules=$($LIBEXEC/check_snmp -P 2c -H $1 -C $2 -o LEFTHAND-NETWORKS-NSM-CLUSTERING-MIB::clusModuleCount.0|cut -d" " -f4)
-#echo "totalmodules=$totalmodules"
+echo "totalmodules=$totalmodules"
 
 # total number of modules in this particular cluster
 clustermodules=$($LIBEXEC/check_snmp -P 2c -H $1 -C $2 -o LEFTHAND-NETWORKS-NSM-CLUSTERING-MIB::clusClusterModuleCount.$CLUSTERINSTANCE|cut -d" " -f4)
-#echo "clustermodules=$clustermodules"
+echo "clustermodules=$clustermodules"
 
 checkcount=0
-
-#cycle through each module. Get the totalsize, check only those who match our clustername, then find the smallest sized module in our cluster, multiply it by
-# the number of modules in our cluster - that should give us our total cluster capacity.
+# Toma modulo del total de modulos del sistema.
 while [ "$checkcount" -lt $totalmodules ]
 do
-ck=$(echo $checkcount + 1 | bc)
-
+ck=$(echo $checkcount + 1 | bc) #suma 1
+# toma el nombre del cluster al cual pertenece el modulo
 ibelongto=$($LIBEXEC/check_snmp -P 2c -H $1 -C $2 -o LEFTHAND-NETWORKS-NSM-CLUSTERING-MIB::clusModuleClusterName.$ck|cut -d" " -f4)
-
-#echo "does $ibelongto equal $clustername?"
+# si es el cluster al que estamos consultando toma el tamaño del modulo
 if [ "$ibelongto" = "$clustername" ] ; then
-
-modtotal=$($LIBEXEC/check_snmp -P 2c -H $1 -C $2 -o LEFTHAND-NETWORKS-NSM-CLUSTERING-MIB::clusModuleUsableSpace.$ck|cut -d" " -f4)
-
-modtotal=$(echo "$modtotal" | bc)
-#echo "stdin errors above...."
-
-if [ checkmodtotal=0 ] ; then
-checkmodtotal=$(echo "$modtotal" | bc)
+	modtotal=$($LIBEXEC/check_snmp -P 2c -H $1 -C $2 -o LEFTHAND-NETWORKS-NSM-CLUSTERING-MIB::clusModuleUsableSpace.$ck|cut -d" " -f4)
+	modtotal=$(echo "$modtotal" | bc)
+	# Busca el modulo mas pequeño para multiplicarlo con la cant. de modulos del cluster y obtener la capacidad total
+	if [ checkmodtotal=0 ] ; then
+		checkmodtotal=$(echo "$modtotal" | bc)
+	fi
+	if [ $modtotal -le $checkmodtotal ] ; then
+		checkmodtotal=$(echo "$modtotal" | bc)
+	fi
 fi
-#echo "Check to see if $modtotal is -le $checkmodtotal"
-if [ $modtotal -le $checkmodtotal ] ; then
-checkmodtotal=$(echo "$modtotal" | bc)
-#echo "setting total to $checkmodtotal"
-fi
-fi
-
 #echo "checkmodtotal is $checkmodtotal"
 checkcount=$(($checkcount+1))
 done
 
 # How many volumes are in this management group? Let's cycle through them all and count the used space of the volume, and its snapshots (but only
 # if they belong to OUR cluster:
+# Toma cantidad de volumenes total
 numbervolumes=$($LIBEXEC/check_snmp -P 2c -H $1 -C $2 -o LEFTHAND-NETWORKS-NSM-CLUSTERING-MIB::clusVolumeCount.0|cut -d" " -f4)
-#echo "Total Volumes in this Management Group is $numbervolumes"
+echo "Total Volumes in this Management Group is $numbervolumes"
 
 addvolumes=1
 totalused=0
@@ -75,54 +75,48 @@ while [ "$addvolumes" -le $numbervolumes ]
 do
 ibelongto=$($LIBEXEC/check_snmp -P 2c -H $1 -C $2 -o LEFTHAND-NETWORKS-NSM-CLUSTERING-MIB::clusVolumeClusterName.$addvolumes|cut -d" " -f4)
 if [ "$ibelongto" = "$clustername" ] ; then
-#echo "Sure does... check replication level, then look to see if there are snapshots"
-repllevel=$($LIBEXEC/check_snmp -P 2c -H $1 -C $2 -o LEFTHAND-NETWORKS-NSM-CLUSTERING-MIB::clusVolumeReplicaCount.$addvolumes|cut -d" " -f4)
-#echo "Got a rep level of $repllevel"
-snapcount=$($LIBEXEC/check_snmp -P 2c -H $1 -C $2 -o LEFTHAND-NETWORKS-NSM-CLUSTERING-MIB::clusVolumeSnapshotCount.$addvolumes|cut -d" " -f4)
-#echo "found $snapcount snapshots"
-snapcheck=1
-# get the used space of the volume for now.
-thisvolumeprov=$($LIBEXEC/check_snmp -P 2c -H $1 -C $2 -o LEFTHAND-NETWORKS-NSM-CLUSTERING-MIB::clusVolumeProvisionedSpace.$addvolumes|cut -d" " -f4)
-#echo "non snap size is $thisvolumeprov"
-# now cycle through all the snapshots, and add their used space together
-while [ "$snapcheck" -le $snapcount ]
-do
-thissnapprov=$($LIBEXEC/check_snmp -P 2c -H $1 -C $2 -o LEFTHAND-NETWORKS-NSM-CLUSTERING-MIB::clusVolumeSnapshotProvisionedSpace.$addvolumes.$snapcheck|cut -d" " -f4)
-#echo "bare snap is $thissnapprov - now add volume plus snapshot"
-thisvolumeprov=$(echo "$thisvolumeprov + $thissnapprov" | bc)
-#echo "This volumes size so far is $thisvolumeprov"
-snapcheck=$(($snapcheck+1))
-done
+	# si el volumen pertenece al cluster al que estamos consultando, ckequea si hay replicas, y si las hay revisa snapshots"
+	repllevel=$($LIBEXEC/check_snmp -P 2c -H $1 -C $2 -o LEFTHAND-NETWORKS-NSM-CLUSTERING-MIB::clusVolumeReplicaCount.$addvolumes|cut -d" " -f4)
+	snapcount=$($LIBEXEC/check_snmp -P 2c -H $1 -C $2 -o LEFTHAND-NETWORKS-NSM-CLUSTERING-MIB::clusVolumeSnapshotCount.$addvolumes|cut -d" " -f4)
+	# echo "se encontraron $snapcount snapshots"
+	snapcheck=1
+	# Toma el espacio utilizado del volumen
+	thisvolumeprov=$($LIBEXEC/check_snmp -P 2c -H $1 -C $2 -o LEFTHAND-NETWORKS-NSM-CLUSTERING-MIB::clusVolumeProvisionedSpace.$addvolumes|cut -d" " -f4)
 
-repllevel=$(echo "$repllevel / 1" | bc)
-#thisvolumeprov=$(echo "$repllevel * $thisvolumeprov" | bc)
-totalused=$(echo "$totalused + $thisvolumeprov" | bc)
-dispgb=$(echo "$thisvolumeprov/1024" | bc)
-dispgb=$(echo "$dispgb/1024" | bc)
-#echo "thisvolume=$dispgb ($repllevel way replication)"
+	# Recorre cada snapshot uniendolas en la variable: thisvolumeprov
+	while [ "$snapcheck" -le $snapcount ]
+	do
+		thissnapprov=$($LIBEXEC/check_snmp -P 2c -H $1 -C $2 -o LEFTHAND-NETWORKS-NSM-CLUSTERING-MIB::clusVolumeSnapshotProvisionedSpace.$addvolumes.$snapcheck|cut -d" " -f4)
+		thisvolumeprov=$(echo "$thisvolumeprov + $thissnapprov" | bc)
+		#echo "This volumes size so far is $thisvolumeprov"
+		snapcheck=$(($snapcheck+1))
+	done
+	repllevel=$(echo "$repllevel / 1" | bc)
+	#thisvolumeprov=$(echo "$repllevel * $thisvolumeprov" | bc)
+	totalused=$(echo "$totalused + $thisvolumeprov" | bc)
+	dispgb=$(echo "$thisvolumeprov/1024" | bc)
+	dispgb=$(echo "$dispgb/1024" | bc)
+	#echo "thisvolume=$dispgb ($repllevel way replication)"
 fi
 addvolumes=$(($addvolumes+1))
 done
 
-#echo "done checking volumes - let's total it up!"
 # Lefthand SNMP reports usage in KB, so divide by 1024
 totalused=$(echo "$totalused / 1024" | bc)
-#echo "total MB $totalused"
-
-# now divide by 1024 to convert MB to GB
+echo "total MB $totalused"
+# divide by 1024 to convert MB to GB
 totalused=$(echo "$totalused / 1024" | bc)
-#echo "total GB $totalused"
+echo "total GB $totalused"
 
-
+# Calcula la capacidad total del cluster multiplicando la cant. de modulos por checkmodtotal calculado prev.
 clustertotal=$(echo "$checkmodtotal * $clustermodules" | bc)
-#echo "clustertotal=$clustertotal"
+echo "clustertotal=$clustertotal"
 
-# There's some overhead to the cluster total due to formating etc etc. Dunno if this is scientific, but this number seemed
+# ???There's some overhead to the cluster total due to formating etc etc. Dunno if this is scientific, but this number seemed
 # to give me a relatively accurate clustersize across a few different builds, so I'm gonna run with .9845
 clustertotal=$(echo "$clustertotal * .9845" | bc)
-clustertotal=$(echo "$clustertotal/1" | bc)
+clustertotal=$(echo "$clustertotal/1" | bc) #redondea con /1
 #echo "newclustertotal=$clustertotal"
-
 clustertotal=$(echo "$clustertotal / 1024" | bc)
 clustertotal=$(echo "$clustertotal / 1024" | bc)
 #echo "clustertotal in GB = $clustertotal"
@@ -151,28 +145,24 @@ ct=$(echo "$ct /1" | bc)
 
 #echo "wp=$wp"
 #echo "ct=$ct"
-#echo "wt= $wt"
-
+#echo "wt=$wt"
 
 cu=$(expr $clustertotal - $clusteravail)
-
 percentfree=$(echo "$percentfree*100" | bc)
 
-
 if [ $cu -ge $ct ] ; then
-echo "CRITICAL - *$clustername is $percentfree % free* | clustersize=$clustertotal available=$clusteravail warning=$wt critical=$ct provisioned=$totalused"
-exit $STATE_CRITICAL
+	echo "CRITICAL - *$clustername is $percentfree % free* | clustersize=$clustertotal available=$clusteravail warning=$wt critical=$ct provisioned=$totalused"
+	exit $STATE_CRITICAL
 
 elif [ $cu -le $wt ] ; then
-echo "OK - $clustername is $percentfree % free | clustersize=$clustertotal available=$clusteravail warning=$wt critical=$ct provisioned=$totalused"
-exit $STATE_OK
+	echo "OK - $clustername is $percentfree % free | clustersize=$clustertotal available=$clusteravail warning=$wt critical=$ct provisioned=$totalused"
+	exit $STATE_OK
 
 elif [ $cu -gt $wt ] ; then
-echo "WARNING - *$clustername is $percentfree % free* | clustersize=$clustertotal available=$clusteravail warning=$wt critical=$ct provisioned=$totalused"
-exit $STATE_WARNING
+	echo "WARNING - *$clustername is $percentfree % free* | clustersize=$clustertotal available=$clusteravail warning=$wt critical=$ct provisioned=$totalused"
+	exit $STATE_WARNING
 
 else
-#echo "problem - No data received from host"
-exit $STATE_UNKNOWN
+	#echo "problem - No data received from host"
+	exit $STATE_UNKNOWN
 fi 
-
